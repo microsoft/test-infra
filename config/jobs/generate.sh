5faad31ee68b4f6dd35ea6d394c2e25ea6b5707b
Brett McLaren <brmclare@microsoft.com>
2020-09-13 22:55:10 +0000

    test job generator
    Signed-off-by: Brett McLaren <brmclare@microsoft.com>

--- config/jobs/generate.sh
+++ config/jobs/generate.sh
@@ -5,7 +5,7 @@ set +x
 
 echo "Creating config directories if they do not exist.."
 
-org=openenclave
+org=openenclave-ci
 
 # Get all build config types
 build_configs=$(yq r $PWD/config.yml build-configs)
@@ -26,7 +26,7 @@ do
         headers=$(cat $PWD/templates/headers/$build_config.yml)
         echo "$headers" > $PWD/$repo/$repo-$build_config.yaml
 
-        if [ "$build_config" = "pre-submits" ] || [ "$build_mode" = "postsubmits" ] 
+        if [ "$build_config" = "pre-submits" ] || [ "$build_config" = "postsubmits" ] 
         then
             echo "  ${org}/${repo}:" >> $PWD/$repo/$repo-$build_config.yaml
         fi
@@ -38,14 +38,26 @@ do
             build_modes=$(yq r $PWD/config.yml $repo.build-modes)
             for build_mode in $build_modes
             do
+
                 build_types=$(yq r $PWD/config.yml $repo.build-types)
                 for build_type in $build_types
-                do 
+                do
+
                     compilers=$(yq r $PWD/config.yml $repo.compilers)
                     for compiler in $compilers
                     do
+
+                        # Shorten for to keep within 63 prow job char limit
+                        if [ "$compiler" = "clang-7" ]
+                        then
+                            compiler_short=c-7
+                        else
+                            compiler_short=$compiler
+                        fi
+
                         # New line seems to not work 100% of the time, just for readability
                         echo $'' >> $PWD/$repo/$repo-$build_config.yaml
+
                         if [ "$build_mode" = "simulation" ]
                         then
 # TODO: Not evaluating correctly, seems the tabs are being injected into the job config which is not what we want,

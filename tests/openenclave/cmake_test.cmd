a5f322cca67caf674f8f49311faedff8bc72d384
Brett McLaren <brmclare@microsoft.com>
2020-10-01 09:12:36 +0000

    more windows validation scripts
    Signed-off-by: Brett McLaren <brmclare@microsoft.com>

--- tests/openenclave/cmake_test.cmd
+++ tests/openenclave/cmake_test.cmd
@@ -1,35 +1,35 @@
 REM Clean up past workspave
-(if exist openenclave rmdir /s/q openenclave) && ^
+(if exist openenclave rmdir /s/q openenclave)
 
 REM Clone openenclave
-git clone --recursive https://github.com/openenclave/openenclave && ^
+git clone --recursive https://github.com/openenclave/openenclave
 
 REM Set up environment
-cd openenclave && mkdir build && ^ cd build && ^
+cd openenclave && mkdir build && cd build
 
 REM Configure build
-vcvars64.bat && cmake .. -G Ninja -DNUGET_PACKAGE_PATH=C:\\Downloads\\prereqs\\nuget -DCPACK_GENERATOR=NuGet -DCMAKE_BUILD_TYPE=Release -DBUILD_ENCLAVES=ON -DLVI_MITIGATION=None -DHAS_QUOTE_PROVIDER=ON && ^
+vcvars64.bat && cmake .. -G Ninja -DNUGET_PACKAGE_PATH=C:\Downloads\prereqs\nuget -DCPACK_GENERATOR=NuGet -DCMAKE_BUILD_TYPE=Release -DBUILD_ENCLAVES=ON -DLVI_MITIGATION=None -DHAS_QUOTE_PROVIDER=ON
 
 REM Build
-vcvars64.bat && ninja -j 1 -v && ^
+vcvars64.bat && ninja -j 1 -v
 
 REM Test
-vcvars64.bat && ctest.exe -V -C Debug --timeout 480 && ^
+vcvars64.bat && ctest.exe -V -C Debug --timeout 480
 
 REM Package Hostverify
-vcvars64.bat && cpack.exe -D CPACK_NUGET_COMPONENT_INSTALL=ON -DCPACK_COMPONENTS_ALL=OEHOSTVERIFY && ^
+vcvars64.bat && cpack.exe -D CPACK_NUGET_COMPONENT_INSTALL=ON -DCPACK_COMPONENTS_ALL=OEHOSTVERIFY
 
 REM Package
-vcvars64.bat && cpack.exe && ^
+vcvars64.bat && cpack.exe
 
 REM Remove past install
-(if exist C:\\oe rmdir /s/q C:\\oe) && ^
+(if exist C:\\oe rmdir /s/q C:\\oe)
 
 REM Install
-nuget.exe install open-enclave -Source %cd%\\openenclave\\build -OutputDirectory C:\\oe -ExcludeVersion && ^
+nuget.exe install open-enclave -Source %cd%\\openenclave\\build -OutputDirectory C:\\oe -ExcludeVersion
 
 REM Test Package installation
-set CMAKE_PREFIX_PATH=C:\\oe\\open-enclave\\openenclave\\lib\\openenclave\\cmake && ^
-cd C:\\oe\\open-enclave\\openenclave\\share\\openenclave\\samples && ^
+set CMAKE_PREFIX_PATH=C:\\oe\\open-enclave\\openenclave\\lib\\openenclave\\cmake
+cd C:\\oe\\open-enclave\\openenclave\\share\\openenclave\\samples
 setlocal enabledelayedexpansion
 REM #TODO AddSamplesTesting

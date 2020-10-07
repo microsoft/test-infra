3ce74c824039c6d6b27dd6070255e2c1443766c6
Brett McLaren <brmclare@microsoft.com>
2020-10-07 01:09:04 +0000

    hack in hernan change
    Signed-off-by: Brett McLaren <brmclare@microsoft.com>

--- images/test/validate.sh
+++ images/test/validate.sh
@@ -3,18 +3,20 @@ set -ex
 
 # CMake Image Validation
 
-for repo in oeedger8r-cpp openenclave
+for repo in openenclave
 do
   # Remove repo if exists
   if [[ -d ./${repo} ]]; then
     rm -rf ./${repo} || sudo rm -rf ./${repo} 
   fi
 
-  git clone --recursive https://github.com/openenclave/${repo}.git  --depth 1 && \
-  cd ${repo}
-  # Iterate through supported compilers
+  git clone --recursive https://github.com/openenclave/openenclave.git
 
-  for compiler in clang-7 clang-8
+  cd ${repo}
+  git fetch origin +refs/pull/*/merge:refs/remotes/origin/pr/* && \
+  git checkout origin/pr/2527
+
+  for compiler in clang-7 #clang-8
   do
     # TODO re-enable RelDebInfo once clang-8 incompatability is fixed
     for build in Debug Release #RelDebInfo

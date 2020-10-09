d7bc238b8c21305373f485344c3b4e6b10819a21
Brett McLaren <brmclare@microsoft.com>
2020-10-09 22:41:47 +0000

    reduce git logging
    Signed-off-by: Brett McLaren <brmclare@microsoft.com>

--- config/jobs/oeedger8r-cpp/jenkins/common.groovy
+++ config/jobs/oeedger8r-cpp/jenkins/common.groovy
@@ -24,9 +24,6 @@ def cmakeBuild( String REPO_NAME, String BUILD_CONFIG ) {
 def checkout( String REPO_NAME ) {
     if (isUnix()) {
         sh  """
-            export GIT_TRACE_PACKET=1
-            export GIT_TRACE=1
-            export GIT_CURL_VERBOSE=1
             rm -rf ${REPO_NAME} && \
             git clone --recursive --depth 1 https://github.com/openenclave/${REPO_NAME} && \
             cd ${REPO_NAME} && \
@@ -38,9 +35,6 @@ def checkout( String REPO_NAME ) {
     }
     else {
         bat """
-            set GIT_TRACE_PACKET=1
-            set GIT_TRACE=1
-            set GIT_CURL_VERBOSE=1
             (if exist ${REPO_NAME} rmdir /s/q ${REPO_NAME}) && \
             git clone --recursive --depth 1 https://github.com/openenclave/${REPO_NAME} && \
             cd ${REPO_NAME} && \
--- config/jobs/openenclave/jenkins/common.groovy
+++ config/jobs/openenclave/jenkins/common.groovy
@@ -52,9 +52,6 @@ def cmakeBuildOE( String REPO_NAME, String BUILD_CONFIG ) {
 def checkout( String REPO_NAME ) {
     if (isUnix()) {
         sh  """
-            export GIT_TRACE_PACKET=1
-            export GIT_TRACE=1
-            export GIT_CURL_VERBOSE=1
             rm -rf ${REPO_NAME} && \
             git clone --recursive --depth 1 https://github.com/openenclave/${REPO_NAME} && \
             cd ${REPO_NAME} && \
@@ -66,9 +63,6 @@ def checkout( String REPO_NAME ) {
     }
     else {
         bat """
-            set GIT_TRACE_PACKET=1
-            set GIT_TRACE=1
-            set GIT_CURL_VERBOSE=1
             (if exist ${REPO_NAME} rmdir /s/q ${REPO_NAME}) && \
             git clone --recursive --depth 1 https://github.com/openenclave/${REPO_NAME} && \
             cd ${REPO_NAME} && \

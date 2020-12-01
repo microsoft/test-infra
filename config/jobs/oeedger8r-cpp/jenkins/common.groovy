30c32abb5877767055342ed20a7e5d2deeed8c31
brmclaren <brmclare@microsoft.com>
2020-12-01 23:46:53 +0000

    switch to declartive build
    Signed-off-by: brmclaren <brmclare@microsoft.com>

--- config/jobs/oeedger8r-cpp/jenkins/common.groovy
+++ config/jobs/oeedger8r-cpp/jenkins/common.groovy
@@ -1,83 +1,16 @@
-// Common oeedger8r-cpp jenkins functions
+// Common oeedger8r-cpp jenkins function
 
-def cmakeBuildoeedger8r(String REPO_NAME, String BUILD_CONFIG, String COMPILER) {
-    if (isUnix()) {
-        sh  """
-            cd ${REPO_NAME} && \
-            mkdir build && cd build &&\
-            cmake .. -G Ninja -DCMAKE_BUILD_TYPE=${BUILD_CONFIG} -Wdev
-            ninja -v
-            ctest --output-on-failure --timeout
-            """
-    } else {
-        bat """
-            cd ${REPO_NAME} && \
-            mkdir build && cd build &&\
-            vcvars64.bat x64 && \
-            cmake.exe .. -G Ninja -DCMAKE_BUILD_TYPE=${BUILD_CONFIG} && \
-            ninja -v -j 4 && \
-            ctest.exe -V --output-on-failure
-            """
-    }
-}
+/** Checkout oeedger8r-cpp, along with merged pull request. If master is instead passed in, don't check out branch
+  * as this is being ran as a validation of master or as a reverse integration test on the test-infra repo.
+**/
 
-def cmakeBuild( String REPO_NAME, String BUILD_CONFIG ) {
-
-    if (isUnix()) {
-        sh  """
-            cd ${REPO_NAME} && \
-            mkdir build && cd build &&\
-            cmake .. -G Ninja -DCMAKE_BUILD_TYPE=${BUILD_CONFIG} -Wdev
-            ninja -v
-            ctest --output-on-failure --timeout ${REPO_NAME}
-            """
-    } else {
-        bat """
-            cd ${REPO_NAME} && \
-            mkdir build && cd build &&\
-            vcvars64.bat x64 && \
-            cmake.exe .. -G Ninja -DCMAKE_BUILD_TYPE=${BUILD_CONFIG} && \
-            ninja -v -j 4 && \
-            ctest.exe -V --output-on-failure --timeout ${CTEST_TIMEOUT_SECONDS}
-            """
-    }
-}
-
-void cleanContainers() {
-    if (isUnix()) {
-        sh  """
-            docker system prune -f
-            """ 
-    } else {
-        bat """
-            docker system prune -f
-            """
-    }
-}
-
-// Clean up environment, do not fail on error.
-def cleanup( String REPO_NAME) {
-    if (isUnix()) {
-        try {
-                sh  """
-                    set +e
-                    rm -rf ${REPO_NAME}
-                    """
-            } catch (Exception e) {
-                // Do something with the exception 
-                error "Program failed, please read logs..."
-            } 
-        
-    }
-}
-
-void checkout( String REPO_NAME, String PULL_NUMBER ) {
+void checkout( String PULL_NUMBER ) {
     if (isUnix()) {
         sh  """
             git config --global core.compression 0 && \
-            rm -rf ${REPO_NAME} && \
-            git clone --recursive --depth 1 https://github.com/openenclave/${REPO_NAME} && \
-            cd ${REPO_NAME} && \
+            rm -rf oeedger8r-cpp && \
+            git clone --recursive --depth 1 https://github.com/openenclave/oeedger8r-cpp && \
+            cd oeedger8r-cpp && \
             git fetch origin +refs/pull/*/merge:refs/remotes/origin/pr/*
             if [[ ${PULL_NUMBER} -ne 'master' ]]; then
                 git checkout origin/pr/${PULL_NUMBER}
@@ -87,67 +20,54 @@ void checkout( String REPO_NAME, String PULL_NUMBER ) {
     else {
         bat """
             git config --global core.compression 0 && \
-            (if exist ${REPO_NAME} rmdir /s/q ${REPO_NAME}) && \
-            git clone --recursive --depth 1 https://github.com/openenclave/${REPO_NAME} && \
-            cd ${REPO_NAME} && \
+            (if exist oeedger8r-cpp rmdir /s/q oeedger8r-cpp) && \
+            git clone --recursive --depth 1 https://github.com/openenclave/oeedger8r-cpp && \
+            cd oeedger8r-cpp && \
             git fetch origin +refs/pull/*/merge:refs/remotes/origin/pr/*
             if NOT ${PULL_NUMBER}==master git checkout origin/pr/${PULL_NUMBER}
             """
     }
 }
 
-def ContainerRun(String imageName, String compiler, String task, String runArgs="") {
-    def image = docker.image(imageName)
-    image.pull()
-    image.inside(runArgs) {
-        dir("${WORKSPACE}/openenclave/build") {
-            Run(compiler, task)
-        }
+/** Build oeedgr8r based on build config, compiler and platform
+  * TODO: Add container support
+  * TODO: Add a switch for compiler and set to env, pass compiler anyways to validate current workflow
+**/
+def cmakeBuildoeedger8r( String BUILD_CONFIG, String COMPILER) {
+    dir ('oeedger8r-cpp/build') {
+        if (isUnix()) {
+            sh  """
+                cd oeedger8r-cpp && \
+                mkdir build && cd build &&\
+                cmake .. -G Ninja -DCMAKE_BUILD_TYPE=${BUILD_CONFIG} -Wdev
+                ninja -v
+                ctest --output-on-failure --timeout
+                """
+        } else {
+            bat """
+                cd oeedger8r-cpp && \
+                mkdir build && cd build &&\
+                vcvars64.bat x64 && \
+                cmake.exe .. -G Ninja -DCMAKE_BUILD_TYPE=${BUILD_CONFIG} && \
+                ninja -v -j 4 && \
+                ctest.exe -V --output-on-failure
+                """
     }
 }
 
-def runTask(String task) {
-    dir("${WORKSPACE}/build") {
-        sh """#!/usr/bin/env bash
-                set -o errexit
-                set -o pipefail
-                source /etc/profile
-                ${task}
-            """
-    }
-}
-
-def Run(String compiler, String task, String compiler_version = "") {
-    def c_compiler
-    def cpp_compiler
-    switch(compiler) {
-        case "cross":
-            // In this case, the compiler is set by the CMake toolchain file. As
-            // such, it is not necessary to specify anything in the environment.
-            runTask(task)
-            return
-        case "clang-7":
-            c_compiler = "clang"
-            cpp_compiler = "clang++"
-            compiler_version = "7"
-            break
-        case "gcc":
-            c_compiler = "gcc"
-            cpp_compiler = "g++"
-            break
-        default:
-            // This is needed for backwards compatibility with the old
-            // implementation of the method.
-            c_compiler = "clang"
-            cpp_compiler = "clang++"
-            compiler_version = "8"
-    }
-    if (compiler_version) {
-        c_compiler += "-${compiler_version}"
-        cpp_compiler += "-${compiler_version}"
-    }
-    withEnv(["CC=${c_compiler}","CXX=${cpp_compiler}"]) {
-        runTask(task);
+// Clean up environment, do not fail on error.
+def cleanup() {
+    if (isUnix()) {
+        try {
+                sh  """
+                    set +e
+                    rm -rf oeedger8r-cpp
+                    """
+            } catch (Exception e) {
+                // Do something with the exception 
+                error "Program failed, please read logs..."
+            } 
+        
     }
 }
 

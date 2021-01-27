32b3ba9fa581ffca2c7ed50eb792f227e2462b9f
brmclare <brmclare@microsoft.com>
2021-01-27 20:36:41 +0000

    ...checkpoint..
    Signed-off-by: brmclare <brmclare@microsoft.com>

--- config/jobs/openenclave/jenkins/common.groovy
+++ config/jobs/openenclave/jenkins/common.groovy
@@ -1,9 +1,8 @@
 // Common openenclave jenkins functions
-// TODO: Unspaghetti THIS ENTIRE FILE :)
+
 /** Checkout openenclave, along with merged pull request. If master is instead passed in, don't check out branch
   * as this is being ran as a validation of master or as a reverse integration test on the test-infra repo.
 **/
-
 void checkout( String PULL_NUMBER="master" ) {
     if (isUnix()) {
         sh  """
@@ -32,7 +31,7 @@ void checkout( String PULL_NUMBER="master" ) {
 }
 
 /* installOpenEnclavePrereqs
-*  Runs ansible configuration on vanilla vm to allow e2e variant
+*  Runs set up configuration configuration on vanilla vm to allow e2e variant checks
 */
 void installOpenEnclavePrereqs() {
     dir ('openenclave') {
@@ -53,6 +52,7 @@ void installOpenEnclavePrereqs() {
     }
 }
 
+// Get C Compiler Flags
 def getCCompiler(String COMPILER="clang-8"){
     switch(COMPILER) {
         case "gcc":
@@ -66,6 +66,7 @@ def getCCompiler(String COMPILER="clang-8"){
     }
 }
 
+// Get CXX Compiler Flags
 def getCXXCompiler(String COMPILER="clang-8"){
     switch(COMPILER) {
         case "gcc":
@@ -79,19 +80,16 @@ def getCXXCompiler(String COMPILER="clang-8"){
     }
 }
 /** Build openenclave based on build config, compiler and platform
-  * TODO: Add container support
 **/
 def cmakeBuildopenenclave( String BUILD_CONFIG="Release", String COMPILER="clang-7", String EXTRA_CMAKE_ARGS ="") {
     dir ('openenclave/build') {
         if (isUnix()) {
 
-            sh  """
-                echo COMPILER IS ${COMPILER}
-                echo BUILD_CONFIG IS ${BUILD_CONFIG}
-                echo EXTRA_CMAKE_ARGS IS ${EXTRA_CMAKE_ARGS}
-                """
             def c_compiler = getCCompiler("${COMPILER}")
             def cpp_compiler = getCXXCompiler("${COMPILER}")
+            
+            printDebug()
+
             withEnv(["CC=${c_compiler}","CXX=${cpp_compiler}"]) {
                 sh  """
                     cmake .. -G Ninja -DCMAKE_BUILD_TYPE=${BUILD_CONFIG} ${EXTRA_CMAKE_ARGS}
@@ -110,22 +108,18 @@ def cmakeBuildopenenclave( String BUILD_CONFIG="Release", String COMPILER="clang
     }
 }
 
-// Common build and package functionality. WIP
-
+// Common build and package functionality.
 def openenclavepackageInstall( String BUILD_CONFIG="Release", String COMPILER="clang-7", String EXTRA_CMAKE_ARGS ="") {
     dir ('openenclave/build') {
-        // Duplicate code here, refactor out once package is complete
         if (isUnix()) {
+            
+            printDebug()
 
-            sh  """
-                echo COMPILER IS ${COMPILER}
-                """
             def c_compiler = getCCompiler("${COMPILER}")
             def cpp_compiler = getCXXCompiler("${COMPILER}")
-            
-            // To do, switch ninja to cpack sometimes?
 
             withEnv(["CC=${c_compiler}","CXX=${cpp_compiler}"]) {
+                //Missing lvi sample test case
                 sh  """
                     sudo ninja -v package
                     sudo ninja -v install
@@ -174,10 +168,15 @@ def checkCI() {
     dir ('openenclave') {
         if (isUnix()) {
             try {
-                    sh  """
-                        echo 'bug here, revisit before go live'
-                        #./scripts/check-ci
-                        """
+                    // This is a hack, migrating docker image repos and we just need the short hand 
+                    // linux version for compatability with legacy repo.
+                    def lin_version = "${params.LINUX_VERSION}" == "Ubuntu-1604" ? "16.04" : "18.04"
+                    def task =  """
+                                echo 'bug here, revisit before go live'
+                                #./scripts/check-ci
+                                """
+                    Run("cross", task)
+
                 } catch (Exception e) {
                     // Do something with the exception 
                     error "Program failed, please read logs..."
@@ -202,24 +201,8 @@ def AArch64GNUBuild( String BUILD_CONFIG="Release") {
                 """
     ContainerRun("oeciteam/oetools-full-${lin_version}", "cross", task, "--cap-add=SYS_PTRACE")
 }
-// Clean up environment, do not fail on error.
-def cleanup() {
-    if (isUnix()) {
-        try {
-                sh  """
-                    sudo rm -rf openenclave || rm -rf openenclave || echo 'Workspace is clean'
-                    sudo rm -rf /opt/openenclave || rm -rf /opt/openenclave || echo 'Workspace is clean'
-                    sudo rm -rf ~/samples || rm -rf ~/samples || echo 'Workspace is clean'
-                    """
-            } catch (Exception e) {
-                // Do something with the exception 
-                error "Program failed, please read logs..."
-            } 
-        
-    }
-}
 
-/// This is copy and pasted from the deprecated openenclave-ci repo and is used for multiphase tests, we should consider cleaning this up and refactoring 
+// This is copy and pasted from the deprecated openenclave-ci repo and is used for multiphase tests, we should consider cleaning this up and refactoring 
 def ContainerRun(String imageName, String compiler, String task, String runArgs="") {
     def image = docker.image(imageName)
     image.pull()
@@ -227,6 +210,15 @@ def ContainerRun(String imageName, String compiler, String task, String runArgs=
         dir("${WORKSPACE}/openenclave/build") {
             Run(compiler, task)
         }
+    }
+}
+
+def Run(String compiler, String task) {
+    def c_compiler = getCCompiler("${compiler}")
+    def cpp_compiler = getCXXCompiler("${compiler}")
+    
+    withEnv(["CC=${c_compiler}","CXX=${cpp_compiler}"]) {
+        runTask(task);
     }
 }
 
@@ -241,37 +233,25 @@ def runTask(String task) {
     }
 }
 
-def Run(String compiler, String task, String compiler_version = "") {
-    def c_compiler
-    def cpp_compiler
-    switch(compiler) {
-        case "cross":
-            // In this case, the compiler is set by the CMake toolchain file. As
-            // such, it is not necessary to specify anything in the environment.
-            runTask(task)
-            return
-        case "clang-7":
-            c_compiler = "clang"
-            cpp_compiler = "clang++"
-            compiler_version = "7"
-            break
-        case "gcc":
-            c_compiler = "gcc"
-            cpp_compiler = "g++"
-            break
-        default:
-            // This is needed for backwards compatibility with the old
-            // implementation of the method.
-            c_compiler = "clang"
-            cpp_compiler = "clang++"
-            compiler_version = "8"
-    }
-    if (compiler_version) {
-        c_compiler += "-${compiler_version}"
-        cpp_compiler += "-${compiler_version}"
-    }
-    withEnv(["CC=${c_compiler}","CXX=${cpp_compiler}"]) {
-        runTask(task);
+def printDebug(){
+    sh  """
+        printenv
+        """
+}
+
+// Clean up environment, do not fail on error.
+def cleanup() {
+    if (isUnix()) {
+        try {
+                sh  """
+                    sudo rm -rf openenclave || rm -rf openenclave || echo 'Workspace is clean'
+                    sudo rm -rf /opt/openenclave || rm -rf /opt/openenclave || echo 'Workspace is clean'
+                    sudo rm -rf ~/samples || rm -rf ~/samples || echo 'Workspace is clean'
+                    """
+            } catch (Exception e) {
+                // Do something with the exception 
+                error "Program failed, please read logs..."
+            } 
     }
 }
 
